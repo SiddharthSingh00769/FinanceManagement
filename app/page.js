cccdace2fe75d53a4804f70fc0267e733274c074
Siddharth Singh <sid018singh@gmail.com>
2025-06-19 17:19:10 +0530

    Half Landing page completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant="destructive">
-      hello!
-    </Button>
+    <div className="mt-40">
+      <HeroSection/>
+    </div>
   );
 }
