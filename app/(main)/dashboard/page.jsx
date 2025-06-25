9de869fbb073035cde5a41df267c9d8210d48cd2
Siddharth Singh <sid018singh@gmail.com>
2025-06-26 01:20:18 +0530

    completing filter feature

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,6 +1,14 @@
+import { getUserAccounts } from '@/actions/dashboard';
+import CreateAccountDrawer from '@/components/create-account-drawer'
+import { Card, CardContent } from '@/components/ui/card'
+import { Plus } from 'lucide-react'
 import React from 'react'
+import AccountCard from './_components/account-card';
 
-const DashboardPage = () => {
+async function DashboardPage() {
+
+  const accounts = await getUserAccounts();
+
   return (
     <div className='px-5'>
       {/* Budget Progress */}
@@ -8,6 +16,20 @@ const DashboardPage = () => {
       {/* Overview */}
 
       {/* Accounts Grid */}
+      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+        <CreateAccountDrawer>
+          <Card className='hover:shadow-md cursor-pointer transition-shadow border-dashed'>
+            <CardContent className='flex flex-col items-center justify-center text-muted-foreground h-full pt-5'>
+              <Plus className='h-10 w-10 mb-2'/>
+              <p className='text-sm font-medium'>Add a new account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+
+        {accounts.length > 0 && accounts.map((account) => {
+          return <AccountCard key={account.id} account={account} />
+        })}
+      </div>
     </div>
   )
 }
