21332909621b7afaf8d79e983e45ec28abc7bbbb
Siddharth Singh <sid018singh@gmail.com>
2025-07-06 11:59:25 +0530

    started inngest email automation

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -12,7 +12,7 @@ const serializeTransaction = (obj) => {
     }
 
     if(obj.amount){
-        serialized.amount = obj.balance.toNumber();
+        serialized.amount = obj.amount.toNumber();
     }
 
     return serialized;
@@ -110,4 +110,25 @@ export async function getUserAccounts() {
 
     const serializedAccount = accounts.map(serializeTransaction);
     return serializedAccount;
+}
+
+export async function getDashboardData() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Get all user transactions
+  const transactions = await db.transaction.findMany({
+    where: { userId: user.id },
+    orderBy: { date: "desc" },
+  });
+
+  return transactions.map(serializeTransaction);
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,37 +1,57 @@
-import { getUserAccounts } from '@/actions/dashboard';
-import CreateAccountDrawer from '@/components/create-account-drawer'
-import { Card, CardContent } from '@/components/ui/card'
-import { Plus } from 'lucide-react'
-import React from 'react'
-import AccountCard from './_components/account-card';
+import { Suspense } from "react";
+import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData } from "@/actions/dashboard";
+import { getCurrentBudget } from "@/actions/budget";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import { DashboardOverview } from "./_components/transaction-overview";
+import AccountCard from "./_components/account-card";
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { BudgetProgress } from "./_components/budget-progres";
 
-async function DashboardPage() {
+export default async function DashboardPage() {
+  const [accounts, transactions] = await Promise.all([
+    getUserAccounts(),
+    getDashboardData(),
+  ]);
 
-  const accounts = await getUserAccounts();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
-    <div className='px-5'>
-      {/* Budget Progress */}
-
-      {/* Overview */}
-
+    <div className="space-y-8">
       {/* Accounts Grid */}
-      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
-          <Card className='hover:shadow-md cursor-pointer transition-shadow border-dashed'>
-            <CardContent className='flex flex-col items-center justify-center text-muted-foreground h-full pt-5'>
-              <Plus className='h-10 w-10 mb-2'/>
-              <p className='text-sm font-medium'>Add a new account</p>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
             </CardContent>
           </Card>
         </CreateAccountDrawer>
-
-        {accounts.length > 0 && accounts.map((account) => {
-          return <AccountCard key={account.id} account={account} />
-        })}
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
       </div>
-    </div>
-  )
-}
 
-export default DashboardPage
+      {/* Budget Progress */}
+      <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
+
+      {/* Dashboard Overview */}
+      <DashboardOverview
+        accounts={accounts}
+        transactions={transactions || []}
+      />
+    </div>
+  );
+}
